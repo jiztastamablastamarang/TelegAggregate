ab8863330872711e33228afcbac62e318d4a5385
Eugene <Jiztastamablastamarang@gmail.com>
2022-06-12 09:11:15 +0800

    working carcass

--- config.py
+++ config.py
@@ -1,23 +1,6 @@
 OFFSET_DAYS = 7
 
 CHANNELS = {
-    "https://t.me/pythonrabota",
-    "https://t.me/remotejobss",
-    "https://t.me/junior_dou_ua",
-    "https://t.me/uajobit",
-    "https://t.me/myjobit",
-    "https://t.me/datasciencejobs",
-    "https://t.me/backendge",
-    "https://t.me/itexpert_vacancies",
-    "https://t.me/hiddengurus",
-    "https://t.me/Remoteit",
-    "https://t.me/remotelist",
-    "https://t.me/digitalbroccoli",
-    "https://t.me/itfreelancers",
-    "https://t.me/theyseeku",
-    "https://t.me/Relocats",
-    "https://t.me/jobs_abroad",
-    "https://t.me/Cvflow",
     "https://t.me/jobsearchIT",
     "",
 }
@@ -27,7 +10,5 @@ KEYWORDS = {
     "golang",
     "#go"
     "go lang",
-    "junior",
-    "trainee",
 }
 
